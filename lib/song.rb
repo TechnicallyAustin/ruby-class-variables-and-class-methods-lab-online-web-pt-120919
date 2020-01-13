8718787ecbf077a554cfc3fb07fd5efeb2adc9cf
Austin Johnson <technicallyaustin@gmail.com>
2020-01-13 20:48:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
   @@artists = []
   @@genres = []
   @@unique_artist = []
+  @@unique_genre = []
   @@count = 0
 
 def initialize(name,artist,genre)
@@ -14,9 +15,13 @@ def initialize(name,artist,genre)
   @artist = artist
   #shovel each artist and Genre into class variables
   @@artists << @artist
-  unless @@artists.include?(@artist) == false 
+  unless @@artists.include?(@artist) do  
     @@unique_artist << @artist
   end
+  unless @@genres.include?(@genres) do 
+    @@unique_genre << @genre
+  end
+    
   @@genres << @genre
   @@total_songs << @name 
   @@count += 1
@@ -28,6 +33,9 @@ def self.count
 end
 
 def self.artists
+  @@unique_artist
+end
+
   
 
 
